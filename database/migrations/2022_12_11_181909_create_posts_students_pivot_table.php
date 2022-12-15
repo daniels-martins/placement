9ef6f4c427a8aebc0d1520d0013958b5c657a053
martins-daniels <danielsmrtns@gmail.com>
2022-12-15 07:38:03 -0800

    a todo for the migration file regarding a manytomany relationship

--- database/migrations/2022_12_11_181909_create_posts_students_pivot_table.php
+++ database/migrations/2022_12_11_181909_create_posts_students_pivot_table.php
@@ -7,30 +7,32 @@ use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('post_student', function (Blueprint $table) {
-           $table->unsignedBigInteger('post_id');
-            $table->id();
-            $table->unsignedBigInteger('student_id');
-            $table->foreignIdFor(Application::class, 'application_id'); //pivot
-            $table->timestamps();
-            
-        });
-    }
+   /**
+    * Run the migrations.
+    *
+    * @return void
+    */
+   public function up()
+   {
+      Schema::create('post_student', function (Blueprint $table) {
+         $table->unsignedBigInteger('post_id');
+         $table->id();
+         $table->unsignedBigInteger('student_id');
+         // error in code generation , this line should have been 
+         $table->unsignedBigInteger('post_id');
+         // bcos it is the post_user table relationship and not an application_user relationship, check model for concurrency and make sure you do this
+         $table->foreignIdFor(Application::class, 'application_id'); //pivot
+         $table->timestamps();
+      });
+   }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('post_student');
-    }
+   /**
+    * Reverse the migrations.
+    *
+    * @return void
+    */
+   public function down()
+   {
+      Schema::dropIfExists('post_student');
+   }
 };
